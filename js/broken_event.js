f850a5b7cb077d83c7f9b1f910cf3cdd87d72723
rommel dalisay <rommelmdalisay@gmail.com>
2024-05-28 13:48:27 +0800

    event object

--- js/broken_event.js
+++ js/broken_event.js
@@ -3,10 +3,12 @@ function clickHandler(e, objId, num, msg) {
     obj.innerHTML = 'DIV ' + num + ' says ' + msg + ' at X postion: ' + e.screenX;
 }
 function yesWrapper(e) {
+    console.log(e.target)
     clickHandler(e, 'heading', 1, 'yes');
     e.target.removeEventListener('click', yesWrapper);
 }
 function noWrapper(e) {
+    console.log(e.target)
     clickHandler(e, 'heading', 2, 'no');
     e.target.removeEventListener('click', noWrapper);
 }
